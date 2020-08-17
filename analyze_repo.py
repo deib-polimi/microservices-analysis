d6093e4576f17b40dbbfe9a953fcd81a669233db
gioenn <giovanniquattrocchi@me.com>
2020-08-17 14:19:34 +0200

    fixes

--- analyze_repo.py
+++ analyze_repo.py
@@ -66,6 +66,8 @@ def clone(repo_url, full_repo_name):
         print('-cloning repo')
         endpoint = 'https://api.github.com/repos/%s' % (full_repo_name,)
         p1 = subprocess.run(['curl', endpoint], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL,)
+        data = json.loads(p1.stdout.decode("utf-8"))
+        print('repo size', '%dM' % (size/1000,))
         if 'size' not in data or data['size'] < 512000:
             git.Git(workdir).clone(repo_url)
         else:
