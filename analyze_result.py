09b238abf8376cf63f8a1671b97e94a9f580306a
gioenn <giovanniquattrocchi@me.com>
2020-08-18 14:16:57 +0200

    fixes

--- analyze_result.py
+++ analyze_result.py
@@ -3,28 +3,45 @@ from os import path
 from pathlib import Path
 from collections import Counter
 
-num_services = 0
-num_ok = 0
-num_files = 0
-num_dockers = 0
-dbs_flat = []
-dbs = []
-langs = []
-langs_flat = []
-polyglot = 0
-excluded_languages = ['css', 'html', 'dockerfile']
+
+KEYS = [ 'dbs', 'servers', 'buses', 'langs', 'gates', 'monitors', 'discos' ]
+NUMS = ["num_%s" % (k,) for k in KEYS]
+NUMS.append("num_files")
+NUMS.append("num_dockers")
+NUMS.append("num_services")
+
+CLEANER = {}
+
+DATA = {}
+
+for key in KEYS:
+    DATA[key] = [[], []]
+    CLEANER[key] = [[''], {}]
+    
+CLEANER['langs'][0] += ['css', 'html', 'jupyternotebook']
+CLEANER['langs'][1].update({'golang' : 'go'})
+
+def clean_data(data):
+    for key in KEYS:
+        for v in data[key]:
+            data[key] = set(data[key]) - set(CLEANER[key][0])
+            syn = CLEANER[key][1]
+            data[key] = [syn[x] if x in syn else x for x in data[key]]
+
+
+num_services = num_ok = num_files = num_dockers = 0
+
 def analyze_data(data):
-    global num_services, num_ok, num_files, num_dockers, dbs, dbs_flat, langs, langs_flat, polyglot
+    global DATA, KEYS, NUMS, num_services, num_ok, num_files, num_dockers
+    clean_data(data)
     num_services += data['num_services']
     num_ok += data['num_services'] > 0
     num_files += data['num_files']
     num_dockers += data['num_dockers']
-    dbs += data['dbs']
-    dbs_flat.append(tuple(sorted(data['dbs'])))
-    data['langs'] = list({'go' if x == 'golang' else x for x in data['langs'] if x not in excluded_languages})
-    polyglot += len(data['langs']) > 1
-    langs += data['langs']
-    langs_flat.append(tuple(sorted(data['langs'])))
+    for key in KEYS:
+        DATA[key][0] += data[key]
+        if data[key]:
+            DATA[key][1].append(tuple(sorted(data[key])))
 
 def analyze_all():
     repos = Path('results').glob('*.json')
@@ -44,12 +61,8 @@ def analyze_all():
 
 analyze_all()
 print(num_services, num_ok, num_files, num_dockers)
-cdbs = Counter(dbs)
-cdbs_flat = Counter(dbs_flat)
-clangs = Counter(langs)
-clangs_flat = Counter(langs_flat)
-print(cdbs.most_common(50))
-print(cdbs_flat.most_common(50))
-print(clangs.most_common(50))
-print(clangs_flat.most_common(50))
-print(polyglot)
+for key in KEYS:
+    print(key)
+    print(Counter(DATA[key][0]).most_common(50))
+    print(Counter(DATA[key][1]).most_common(50))
+    print('---')
