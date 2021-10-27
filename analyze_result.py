58cdda52a733c35f98404114418a12316e1b374a
gioenn <giovanniquattrocchi@me.com>
2021-10-27 11:05:00 +0200

    last update

--- analyze_result.py
+++ analyze_result.py
@@ -7,10 +7,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy import stats
 import sys
-from itertools import combinations 
+import pickle
+import csv
+
+from itertools import combinations, product
 
 with open('colors.csv') as colors_files:
-    colors = colors_files.read().splitlines()
+    COLORS = colors_files.read().splitlines()
 
 KEYS = [ 'dbs', 'servers', 'buses', 'langs', 'gates', 'monitors', 'discos', 'images']
 NONSIZE_KEYS = ['images']
@@ -21,6 +24,9 @@ SIZE_KEYS.append("num_services")
 SIZE_KEYS.append("num_ms")
 SIZE_KEYS.append("size")
 SIZE_KEYS.append("avg_size_service")
+SIZE_KEYS.append("commiters")
+SIZE_KEYS.append("shared_dbs")
+
 SIZES = {k: [] for k in SIZE_KEYS}
 
 CLEANER = {}
@@ -29,47 +35,71 @@ CLEANER = {}
 DATA = {}
 
 for key in KEYS:
-    DATA[key] = [[], []]
+    DATA[key] = [[], [], []]
     CLEANER[key] = [[''], {}]
 
-CLEANER['langs'][0] += ['css', 'html', 'jupyternotebook']
-CLEANER['langs'][1].update({'golang' : 'go'})
+CLEANER['langs'][0] += ['css', 'html', 'jupyternotebook', 'vue', 'dockerfile', 'scratch', 'bash', 'shell', 'makefile']
+CLEANER['langs'][1].update({'golang' : 'go', 'gcc' : 'c', 'cmake': 'c'})
 
 CLEANER['images'][0] += ['base']
+CLEANER['servers'][0] += ['mongoose', 'zookeeper']
+CLEANER['dbs'][0] += ['max', 'zookeeper', 'db']
+CLEANER['dbs'][1].update({'sql' : 'mysql'})
+
+CLEANER['monitors'][0] += ['monitoring']
+CLEANER['gates'][0] += ['gateway', 'loadbalancer', 'loadbalancing']
+
+#CLEANER['gates'][1].update({'gateway' : '(G) nginx', 'loadbalancer': '(G) zuul', 'loadbalancing': ' (G) kong'}
+CLEANER['gates'][1].update({'nginx' : ' nginx (G)', 'zuul': 'zuul (G)', 'kong' : 'kong (G)', 'linkerd' : 'linkerd (G)'})
 
 
 def clean_data(data):
     for key in KEYS:
-        for v in data[key]:
-            data[key] = set(data[key]) - set(CLEANER[key][0])
-            syn = CLEANER[key][1]
-            data[key] = [syn[x] if x in syn else x for x in data[key]]
-    data['num_ms'] = data['num_services']-data['num_dbs']-data['num_buses']-data['num_discos']-data['num_monitors']-data['num_gates']
+        data[key] = set(data[key]) - set(CLEANER[key][0])
+        syn = CLEANER[key][1]
+        data[key] = [syn[x] if x in syn else x for x in data[key]]
 
+    data['num_ms'] = max(2, data['num_services']-data['num_dbs']-data['num_buses']-data['num_discos']-data['num_monitors']-data['num_gates'])
 
-num_services = num_ok = num_files = num_dockers = num_compose = 0
-
-
-dockers = []
 def analyze_data(data):
-    global DATA, KEYS, NUMS, num_services, num_ok, num_files, num_dockers, num_compose
+    global DATA, KEYS
     clean_data(data)
-    if not data['num_dockers'] or data['num_dockers'] > 250:
+    if data['num_dockers'] > 30:
         return
-    num_services += data['num_services']
-    num_ok += data['num_dockers'] > 0
-    num_files += data['num_files']
-    num_dockers += data['num_dockers']
-    num_compose += data['num_services'] > 0
-    dockers.append(data['num_dockers'])
     for key in KEYS:
         DATA[key][0] += data[key]
         if data[key]:
             DATA[key][1].append(tuple(sorted(data[key])))
+        DATA[key][2].append(tuple(sorted(data[key])))
+
     for key in SIZE_KEYS:
         SIZES[key].append(data[key])
+    
 
 def analyze_all():
+    global DATA, SIZES
+    if path.exists('temp/SIZES') and path.exists('temp/DATA'):
+        print('retriving from disk')
+        with open('temp/SIZES', 'rb') as f:
+            SIZES = pickle.load(f)
+        with open('temp/DATA', 'rb') as f:
+            DATA = pickle.load(f)
+        return
+    
+    include = set()
+    
+    with open('include.csv', newline='') as f:
+            reader = csv.reader(f, delimiter=';')
+            while True:
+                try:
+                    line = next(reader)
+                    if line[2] == 'D' or line[2] == 'M':
+                        include.add(line[1])
+                except UnicodeDecodeError as e:
+                    print(e)
+                except StopIteration:
+                    break
+    
     repos = Path('results').glob('*.json')
     i = j = 0
     for source in repos:
@@ -77,11 +107,18 @@ def analyze_all():
         try:
             with open(str(source)) as json_file:
                 data = json.load(json_file)
-                analyze_data(data)
+                if data['url'] and data['url'] in include:
+                    analyze_data(data)
+                
         except (UnicodeDecodeError, json.decoder.JSONDecodeError):
             i += 1
             pass
-    
+
+    with open('temp/SIZES', 'wb') as f:
+        pickle.dump(SIZES, f)
+    with open('temp/DATA', 'wb') as f:
+        pickle.dump(DATA, f)
+    print('writed on disk')
     print("TOTAL", j, "ERRORS", i)
 
 
@@ -89,13 +126,15 @@ def color_with_alpha(hex, alpha):
     hex = hex.lstrip('#')
     return [int(hex[i:i+2], 16)/256 for i in (0, 2, 4)] + [alpha]
 
-def plot_scatter(name, *data, scale='linear', xlabel='', ylabel='', legend=[], colors=[]):
+def plot_scatter(name, *data, x=[], scale='linear', xlabel='', ylabel='', legend=[], colors=[]):
     alpha = 0.8
+    if not x:
+        x = range(len(d))
     for i,d in enumerate(data):
         if colors:
-            plt.scatter(range(len(d)), d, s=500, marker='.', color=color_with_alpha(colors[i], alpha))
+            plt.scatter(x, d, s=500, marker='.', color=color_with_alpha(colors[i], alpha))
         else:
-            plt.scatter(range(len(d)), d, s=500, marker='.')
+            plt.scatter(x, range(len(d)), d, s=500, marker='.')
 
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
@@ -119,7 +158,7 @@ def create_hist(name, b, *data, xlabel='', ylabel='', interval=False, legend=[],
     x.append(f'≤{bin_end}')
     for bin_start, bin_end in bin_counts[1:-1]:
         if interval:
-            x.append(f'{bin_start}-{bin_end}')
+            x.append(f'{bin_start+1}-{bin_end}')
         else:
             x.append(f'{bin_end}')
     bin_start, _ = bin_counts[-1]
@@ -139,7 +178,6 @@ def create_hist(name, b, *data, xlabel='', ylabel='', interval=False, legend=[],
         else:
             rect = plt.bar(x_pos, y, width)       
         #autolabel(rect)
-      
 
     plt.legend(legend)
     plt.savefig(f'plots/{name}.pdf',bbox_inches='tight')
@@ -147,8 +185,8 @@ def create_hist(name, b, *data, xlabel='', ylabel='', interval=False, legend=[],
     plt.clf()
 
 def plot_bar(name, *data, ticks=[], xlabel='', ylabel='', legend=[], colors=[]):
-    l = len(data)
     width = 0.27
+    l = len(data)
     for j, y in enumerate(data):
         x_offset = (j - l / 2) * width + width / 2
         x_pos = [i+x_offset for i, _ in enumerate(y)]
@@ -165,43 +203,209 @@ def plot_bar(name, *data, ticks=[], xlabel='', ylabel='', legend=[], colors=[]):
     plt.clf()
 
 def plot_barh(name, *data, ticks=[], xlabel='', ylabel='', legend=[], colors=[]):
-    l = len(data)
     width = 0.50
-    for j, y in enumerate(data):
-        x_offset = (j - l / 2) * width + width / 2
-        x_pos = [i+x_offset for i, _ in enumerate(y)]
-        if colors:
-            rect = plt.barh(x_pos, y, width, color=colors[j])
-        else:
-            rect = plt.barh(x_pos, y, width)       
+    l = len(data)
+    for d in data:
+        for j, y in enumerate(data):
+            y = list(reversed(y))
+            x_offset = (j - l / 2) * width + width / 2
+            x_pos = [i+x_offset for i, _ in enumerate(y)]
+            if colors:
+                rect = plt.barh(x_pos, y, width, color=colors[j])
+            else:
+                rect = plt.barh(x_pos, y, width)       
     plt.legend(legend)
-    plt.yticks([i for i, _ in enumerate(ticks)], ticks)
+    plt.yticks([i for i, _ in enumerate(ticks)], list(reversed(ticks)))
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.savefig(f'plots/{name}.pdf',bbox_inches='tight')
     plt.cla()
     plt.clf()
 
+def plot_pie(name, b, data, colors=[], labels=None, interval=True):
+    def label(pct, data):
+        val = int(pct/100*sum(data))
+        return "{:.1f}%".format(pct)
+
+    data, bins = np.histogram(data, bins=b)
+    bin_counts = list(zip(bins, bins[1:]))
+    x = []
+    _, bin_end = bin_counts[0]
+    x.append(f'≤{bin_end}')
+    for bin_start, bin_end in bin_counts[1:-1]:
+        if interval:
+            x.append(f'{bin_start+1}-{bin_end}')
+        else:
+            x.append(f'{bin_end}')
+    bin_start, _ = bin_counts[-1]
+    x.append(f'>{bin_start}')
+    if colors:
+        rect = plt.pie(data, labels=x if not labels else labels, colors=colors, autopct=lambda pct: label(pct, data))
+    else:
+        rect = plt.pie(data, labels=x if not labels else labels, autopct=lambda pct: label(pct, data))
+
+    plt.savefig(f'plots/{name}.pdf',bbox_inches='tight')
+    plt.cla()
+    plt.clf()
+
+plt.figure(num=None, figsize=(16, 9), dpi=80, facecolor='w', edgecolor='k')
+
+def nice_colors(*indexes):
+    return [color for i, color in enumerate(COLORS) if i in indexes]
+
 def plots():
     font = {'family' : 'normal',
         'weight' : 'normal',
-        'size'   : 22}
-
-    plt.figure(num=None, figsize=(16, 9), dpi=80, facecolor='w', edgecolor='k')
+        'size'   : 26}
 
     matplotlib.rc('font', **font)
 
-    plot_scatter('size', [x/1000 for x in SIZES['size'] if x < 10**6], [x/1000 for x in SIZES['avg_size_service'] if x < 10**6], legend=['Project size', 'Microservice size'], xlabel='Repositories', ylabel='Size (MB)', colors=colors) 
-    #create_hist(SIZES['size'], [0, 10**3, 10**4, 10**5, sys.maxsize])
-    create_hist('num_services', [1, 3, 5, 7, 10, 15, 20, sys.maxsize], SIZES['num_dockers'], SIZES['num_services'], SIZES['num_ms'], interval=True, legend=['# Dockerfile', '# Compose services', '# Microservices'], ylabel='Occurences', colors=colors)
-    #create_hist(, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, sys.maxsize])
-    #create_hist(SIZES['num_services'], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, sys.maxsize])
-    #create_hist(SIZES['avg_size_service'], [0, 10**3, 5*10**3, 10**4, sys.maxsize])
+
+    create_hist('size', [1, 7, 15, 25, 50, sys.maxsize], [x/1000 for x in SIZES['size']], [x/1000 for x in SIZES['avg_size_service']], interval=True, legend=['Project size', 'Microservice size'], ylabel='Occurrences', xlabel='Size (MB)', colors=nice_colors(0, 5)) 
+    create_hist('num_services', [1, 4, 6, 8, 10, 15, 20, sys.maxsize], SIZES['num_dockers'], SIZES['num_services'], SIZES['num_ms'], interval=True, legend=['# Dockerfile', '# Compose services', '# Microservices'], ylabel='Occurrences', colors=nice_colors(0, 3, 5))
+    
+    #plot_scatter('size-services', [x/1000 for x in SIZES['size']], x=[x/1000 for x in SIZES['avg_size_service']], ylabel='Project Size (MB)', xlabel='Avg Service Size (MB)', colors=nice_colors(0, 5)) 
+
+    imagescomb = []
+    for data in DATA['images'][1]: 
+        imagescomb += [tuple(sorted(x)) for x in combinations(set(data), 2)]
+        imagescomb += [tuple(sorted(x)) for x in combinations(set(data), 3)]
+    print(DATA['images'][1])
+    data, labels = [], []
+    for x in Counter(imagescomb).most_common(20):
+        data.append(x[1])
+        images = []
+        for i in x[0]:
+            images.append(i.split('/')[-1])
+        labels.append('+'.join(images))
+
+    plot_barh('images-comb', data, ticks=labels, xlabel='Occurrences', ylabel='', legend=[], colors=COLORS)
+
+    data, labels = [], []
+    for x in Counter(DATA['images'][0]).most_common(20):
+        data.append(x[1])
+        label = x[0]
+        if '/' in label:
+            parts = x[0].split('/')
+            #label = f'{parts[0]}/../{parts[-1]}'
+            label = parts[-1]
+        labels.append(label)
+    plot_barh('images', data, ticks=labels, xlabel='Occurrences', ylabel='', legend=[], colors=COLORS)
+    print(labels, data)
+    data, labels = [], []
+
+    for x in Counter(DATA['langs'][0]).most_common(20):
+        data.append(x[1])
+        label = x[0]
+        labels.append(label)
+
+    plot_barh('langs', data, ticks=labels, xlabel='Occurrences', ylabel='', legend=[], colors=COLORS)
+
+    comb = []
+    for data in DATA['langs'][1]:
+        comb += [tuple(sorted(x)) for x in combinations(set(data), 2)]
+        comb += [tuple(sorted(x)) for x in combinations(set(data), 3)]
+
+
+    data, labels = [], []
+    for x in Counter(comb).most_common(10):
+        data.append(x[1])
+        labels.append('+'.join(x[0]))
+    plot_barh('langs-comb', data, ticks=labels, xlabel='Occurrences', ylabel='', legend=[], colors=COLORS)
+    plot_pie('committers', [1, 3, 5, 8, sys.maxsize], [x/max(1,SIZES['num_ms'][i]) for i,x in enumerate(SIZES['commiters'])], labels=['Micro (≤3)', 'Small (4-5)', 'Medium (6-8)', 'Large (>8)'],  interval=True, colors=COLORS[1:])
+    create_hist('num_langs', [1, 2, 3, 4, 5,  sys.maxsize], [x/max(1,SIZES['num_langs'][i]) for i, x in enumerate(SIZES['num_ms'])], legend=['# Languages', '# Microservices'], interval=False , ylabel='Occurences', colors=nice_colors(0, 3, 5))
+    plot_barh('langs-comb', data, ticks=labels, xlabel='Occurrences', ylabel='', legend=[], colors=COLORS)
+
+    data, labels = [], []
+    for x in Counter(DATA['buses'][0]).most_common(20):
+        data.append(x[1])
+        label = x[0]
+        labels.append(label)
+
+    plot_barh('buses', data, ticks=labels, xlabel='Occurrences', ylabel='', legend=[], colors=COLORS)
+
+    data, labels = [], []
+    for x in Counter(DATA['discos'][0]+DATA['gates'][0]).most_common(20):
+        data.append(x[1])
+        label = x[0]
+        labels.append(label)
+
+    plot_barh('disco+gates', data, ticks=labels, xlabel='Occurrences', ylabel='', legend=[], colors=COLORS)
+
+    data, labels = [], []
+    for x in Counter(DATA['monitors'][0]).most_common(20):
+        data.append(x[1])
+        label = x[0]
+        labels.append(label)
+
+    plot_barh('monitors', data, ticks=labels, xlabel='Occurrences', ylabel='', legend=[], colors=COLORS)
+
+    data, labels = [], []
+    for x in Counter(SIZES['shared_dbs']).most_common(2):
+        data.append(x[1])
+        label = x[0]
+        labels.append(label)
+    plot_barh('shared_dbs', data, ticks=labels, xlabel='Occurrences', ylabel='', legend=[], colors=COLORS)
+
+def tables():
+
+    servers = DATA['servers'][2]
+    dbs = DATA['dbs'][2]
+
+    comb = []
+    for i, s in enumerate(servers):
+        comb  += [tuple(set(x)) for x in product(s, dbs[i])]
+
+    mostservers = [x for x,_ in Counter(DATA['servers'][0]).most_common(10)]
+    mostdb = [x for x,_ in Counter(DATA['dbs'][0]).most_common(10)]
+    print(Counter(DATA['servers'][0]).most_common(10))
+    print(Counter(DATA['dbs'][0]).most_common(10))
+
+    comb = []
+
+    for i, s in enumerate(servers):
+        s = set(s) & set(mostservers)
+        d = set(dbs[i]) & set(mostdb)
+        comb  += [tuple(x) for x in product(s, d)]
+
+    data = Counter(comb).most_common(100)
+
+    print(data)
+
+    def search_tuple(t, data):
+        for t1, v in data:
+            if t1 == t:
+                return v
+        return 0
+
+    for d in mostdb:
+        print(f'& \\textit{{{d}}}', end=' ')
+
+    print('\\\\ \\hline')
+    totdb = [0] * 10
+    for s in mostservers:
+        print(f'\\textit{{{s}}}', end=' ')
+        tots = 0
+        for i, d in enumerate(mostdb):
+            v = search_tuple((s,d), data)
+            tots += v
+            totdb[i] += v
+            print(f'& ${search_tuple((s,d), data)}$', end=' ')
+        print(f' & ${tots}$\\\\ \\hline')
+
+    for v in totdb:
+        print(f' & ${v}$', end=' ')
+
+    print(f'\\\\ \\hline')
 
 
 analyze_all()
-print(num_services, num_ok, num_files, num_dockers, num_compose)
-
+print('all:', len(SIZES['num_services']), \
+    '#services:', sum(SIZES['num_services']), \
+    '#ok:', len([None for x in SIZES['num_services'] if x > 2]), \
+    '#files:', sum(SIZES['num_files']), \
+    '#dockers:', sum(SIZES['num_dockers']), \
+    '#compose:', len([None for x in SIZES['num_services'] if x > 0]))
 '''
 for key in KEYS:
     print(key)
@@ -210,26 +414,24 @@ for key in KEYS:
     print('---')
 
 '''
+
 plots()
+tables()
+print("\n***STATS***\n")
+for k, v in SIZES.items():
+    print(k.upper())
+    a = np.array(v)
+    print('mean', np.mean(a), 'std', np.std(a), 'min', np.min(a), 'max', np.max(a), np.percentile(a, 75))
 
-imagescomb = []
-for data in DATA['images'][1]:
-    imagescomb += [tuple(set(x)) for x in combinations(data, 2)]
+
+a =  np.array([x/max(1,SIZES['num_langs'][i]) for i, x in enumerate(SIZES['num_ms'])])
+print('mean', np.mean(a), 'std', np.std(a), 'min', np.min(a), 'max', np.max(a), np.percentile(a, 30))
+
+print(Counter(DATA['discos'][0]).most_common(20))
+
+print(Counter(DATA['monitors'][1]).most_common(20))
 
 
-data, labels = [], []
-for x in Counter(imagescomb).most_common(20):
-    data.append(x[1])
-    labels.append('-'.join(x[0]))
-plot_barh('images+comb', data, ticks=labels, xlabel='Occurrences', ylabel='', legend=[], colors=colors)
+print(len([x for x in SIZES['shared_dbs'] if x]))
 
-data, labels = [], []
-for x in Counter(DATA['images'][0]).most_common(20):
-    data.append(x[1])
-    label = x[0]
-    if '/' in label:
-        parts = x[0].split('/')
-        label = f'{parts[0]}/../{parts[-1]}'
-    labels.append(label)
-plot_barh('images', data, ticks=labels, xlabel='Occurrences', ylabel='', legend=[], colors=colors)
-
+print(max([x/max(1,SIZES['num_ms'][i]) for i,x in enumerate(SIZES['commiters'])]))
